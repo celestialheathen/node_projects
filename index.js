7129bad764656100b1ec3afec5d4f21fc18b7f86
celestialheathen <jingchaoyu@hotmail.com>
2021-06-02 18:52:38 -0400

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const Logger = require('./logger')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const mylogger = new Logger()
+const server = http.createServer((req, res) => {
+  // if (req.url === '/') {
 
-mylogger.on('message', (data) => console.log('Called Listener:', data))
+  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+  //     res.writeHead(200, { 'Content-Type': 'text/html'})
+  //     res.end(content)
+  //   })
 
-mylogger.log('Hello world')
+  // }
+
+  // if (req.url === '/about') {
+
+  //   fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+  //     res.writeHead(200, { 'Content-Type': 'text/html'})
+  //     res.end(content)
+  //   })
+
+  // }
+
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
